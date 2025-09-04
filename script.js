199581346adb6e67df6fa5624a949c1dd86a07c3
jabuti <guilhermeoliveirfm@gmail.com>
2025-09-04 17:27:29 -0300

    Update v1.4

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ window.transicaoPagina = function(href) {
     document.querySelector('body').style.opacity = 0;
     setTimeout(function() {
         window.location.href = href;
-    }, 25)
+    }, 100)
 }
