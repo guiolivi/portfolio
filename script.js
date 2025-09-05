d062830d868a2f304384337161a43d969bac631f
jabuti <guilhermeoliveirfm@gmail.com>
2025-09-05 17:26:34 -0300

    Update v1.4.1

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1;
-})
+let main = document.querySelector('#main');
+
+document.addEventListener('DOMContentLoaded', function() {
+    requestAnimationFrame(() => {
+        main.style.opacity = 1;
+    });
+});
 
 window.transicaoPagina = function(href) {
-    document.querySelector('body').style.opacity = 0;
+    main.style.opacity = 0;
     setTimeout(function() {
         window.location.href = href;
-    }, 100)
+    }, 200);
 }
